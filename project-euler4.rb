235ac822cb52fcf265f9c274e11fad06412f0bed
Lara Finnegan <lcfinnegan@gmail.com>
2015-04-05 22:46:11 -0400

    refactored solution 4

--- project-euler4.rb
+++ project-euler4.rb
@@ -1,11 +1,11 @@
+
 def largest_palindrome_product(n)
 	max_divisor = ("9" * n).to_i
 	max = max_divisor ** 2
 	min_divisor = ("1" + ("0" * (n - 1))).to_i
-	remainder = 1
 
 	while max > min_divisor ** 2
-		if is_palindrome?(max.to_s)
+		if is_palindrome?(max)
 			divisor_a = min_divisor
 			until divisor_a == max_divisor
 				remainder = max % divisor_a
@@ -19,8 +19,7 @@ def largest_palindrome_product(n)
 end
 
 def is_palindrome?(n)
-	return true if n.size <= 1
-	n[0] == n[-1] ? is_palindrome?(n[1..-2]) : (return false)
+	n == n.to_s.reverse.to_i
 end 
 
 p largest_palindrome_product(3)
