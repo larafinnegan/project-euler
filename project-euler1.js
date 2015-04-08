5de0834d44d9f397556a212b8e7a327c1918b536
Lara Finnegan <lcfinnegan@gmail.com>
2015-04-07 23:07:22 -0400

    tabification

--- project-euler1.js
+++ project-euler1.js
@@ -3,20 +3,19 @@
 
 function problemOne(limit) {
     var sum = 0;
-    if (limit>1000 || limit<1 || isNaN(limit)) {
+    if (limit > 1000 || limit < 1 || isNaN(limit)) {
 		console.log("That's not a number between 1 and 1000!");
 	}
     else {
         for (var i = limit; i > 0; i--) {
-			  if (i % 3 === 0 || i % 5 === 0) {
-			     sum += i;
-			  }   
+			if (i % 3 === 0 || i % 5 === 0) {
+			    sum += i;
+			}   
         }
         console.log(sum);
     }
 }
 
-//Gotta QA!
     problemOne(0.3);
 	problemOne(-1);
 	problemOne(0);
