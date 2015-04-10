5d0e99cf0715e5de86156a56cf0c911cefc8700e
Lara Finnegan <lcfinnegan@gmail.com>
2015-04-09 20:27:38 -0400

    refactored 6

--- project-euler6.rb
+++ project-euler6.rb
@@ -1,13 +1,12 @@
+require 'benchmark'
+
 def sumsquares(n, sum = 0)
   return sum if n == 0
-  sumsquares( n-1, sum += (n ** 2))
+  sumsquares(n - 1, sum += (n ** 2))
 end
 
 def squaresum(n)
-  sum = 0
-  1.upto(n) { |i| sum += i }
-  sum ** 2
+  ((1..n).reduce(:+)) ** 2
 end
 
-
-p squaresum(100) - sumsquares(100)
+p squaresum(1000) - sumsquares(1000)
